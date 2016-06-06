fffcd84684aca4a82b80237618c04d7dbbba21f4
Manuel Schiller <manuel.schiller@bmw-carit.de>
2016-06-06 08:10:45 +0200

    added InputStream concept
    Change-Id: Ib60624bd4fce97f5f79a07dd6961c49bc71dd6f2

---
+++ include/muesli/InputStream.h
@@ -0,0 +1,53 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2016 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+#ifndef MUESLI_INPUTSTREAM_H
+#define MUESLI_INPUTSTREAM_H
+
+#include <boost/concept_check.hpp>
+
+namespace muesli
+{
+namespace concepts
+{
+
+template <class Stream>
+struct InputStream
+{
+    using Char = typename Stream::Char;
+
+    BOOST_CONCEPT_USAGE(InputStream)
+    {
+        // extract single character from stream
+        Char c = stream.get();
+
+        // extract `size` characters from stream
+        stream.get(s, size);
+    }
+
+private:
+    Stream stream;
+    Char* s;
+    std::size_t size;
+};
+
+} // namespace concepts
+} // namespace muesli
+
+#endif // MUESLI_INPUTSTREAM_H
---
+++ tests/InputStreamTest.cpp
@@ -0,0 +1,38 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2016 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include <boost/concept_check.hpp>
+
+#include <muesli/InputStream.h>
+
+// implements InputStream concept
+class MockInputStream
+{
+public:
+    using Char = char;
+    MOCK_METHOD0(get, char());
+    MOCK_METHOD2(get, void(char* s, std::size_t size));
+};
+
+TEST(InputStreamTest, ConceptCheck)
+{
+    BOOST_CONCEPT_ASSERT((muesli::concepts::InputStream<MockInputStream>));
+}
