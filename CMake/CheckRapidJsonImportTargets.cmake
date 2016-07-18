bcd2ac25bd499af8915061e1d120a649d42e6e71
Christian Hoff <christian.hoff@bmw-carit.de>
2016-07-18 15:10:22 +0200

    clean up rapidjson integration in build scripts
    Change-Id: I7e7a25345e520b7a79355dc0709bd1f584a1a8d5

--- CMake/CheckRapidJsonImportTargets.cmake
+++ CMake/CheckRapidJsonImportTargets.cmake
@@ -3,10 +3,9 @@ message(STATUS "variable RAPIDJSON_INCLUDE_DIRS=${RAPIDJSON_INCLUDE_DIRS}")
 
 if(NOT TARGET rapidjson::rapidjson)
     message(STATUS "rapidjson::rapidjson target not defined. Creating IMPORTED target.")
-    add_library(rapidjson::rapidjson SHARED IMPORTED GLOBAL)
+    add_library(rapidjson::rapidjson INTERFACE IMPORTED GLOBAL)
     set_property(TARGET rapidjson::rapidjson  PROPERTY
         INTERFACE_INCLUDE_DIRECTORIES ${RAPIDJSON_INCLUDE_DIRS}
     )
-
 endif(NOT TARGET rapidjson::rapidjson)
 
