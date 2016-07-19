6d0bd92448710a364e334857a2ea59e3e9890b35
Manuel Schiller <manuel.schiller@bmw-carit.de>
2016-07-19 15:41:41 +0200

    added MetaLazyEval
    Change-Id: I4c18f1073d46c3e48acebc74086ec2074c2f0a7c

---
+++ include/muesli/detail/MetaLazyEval.h
@@ -0,0 +1,43 @@
+/*
+ * #%L
+ * %%
+ * Copyright (C) 2011 - 2016 BMW Car IT GmbH
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+#ifndef MUESLI_DETAIL_METALAZYEVAL_H_
+#define MUESLI_DETAIL_METALAZYEVAL_H_
+
+#include <type_traits>
+
+namespace muesli
+{
+namespace detail
+{
+
+template <typename Head, typename... Tail>
+struct MetaLazyEval : std::conditional_t<Head::value, MetaLazyEval<Tail...>, std::false_type>
+{
+};
+
+template <typename Head>
+struct MetaLazyEval<Head> : Head
+{
+};
+
+} // namespace detail
+} // namespace muesli
+
+#endif // MUESLI_DETAIL_METALAZYEVAL_H_
